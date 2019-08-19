79f7e1220d2754d9442729a80805c07c50da03f3
SafaryanAlexandr <11220099.as@gmail.com>
2019-08-19 22:47:51 +0300

    задание №1.2

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,10 +1,14 @@
 const activeWindow = document.getElementById('modal_main');
-activeWindow.className = 'modal modal_active';
-const clickElement = document.getElementById('modal_success');
+activeWindow.classList.add('modal_active');
 document.querySelector('.show-success').onclick = function() {
-    (document.getElementById('modal_success')).className = 'modal modal_active';
+    (document.getElementById('modal_success')).classList.add('modal_active');
+    activeWindow.classList.remove('modal_active');
     };
-const closeWindow = document.querySelector('.modal__close');
-closeWindow.onclick = function() {
-    activeWindow.className = 'modal';
+const elements = document.querySelectorAll('.modal__close_times');
+for (i= 0; i < elements.length; i++) {
+    let closeWindow = elements[i];
+    closeWindow.onclick = function() {
+        activeWindow.className = 'modal';
+        document.getElementById('modal_success').className = 'modal';
+   }
 }
