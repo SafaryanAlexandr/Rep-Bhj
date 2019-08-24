2d70299f0d48d01ac6af33145d7e3b5d028eb418
SafaryanAlexandr <11220099.as@gmail.com>
2019-08-24 22:57:02 +0300

    задание №3 повышенной сложности

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -6,26 +6,41 @@ document.querySelector('.slider__arrow_prev').onclick = function(){
 };
 const slides = document.querySelectorAll('.slider__item');// все слайды
 function activeSlide(n) {
-    for (i= 1; i < slides.length; i++) {
+    for (i= 0; i < slides.length; i++) {
         slides[i].className = 'slider__item';
+        dots[i].className = 'slider__dot';
     }
     slides[n].className = 'slider__item slider__item_active';
+    dots[n].className = 'slider__dot slider__dot_active';
 }
-let number = 1;
+let number = 0;
 function nextSlider() {
-    if(number != slides.length){
+    if(number < (slides.length -1)){
         number++;  
     } else {
-        number = 1;
+        number = 0;
     }
     activeSlide(number);
 }
 function prevSlider() {
-    if(number <= 1){
-        number =  slides.length;
+    if(number === 0){
+        number =  slides.length - 1;
     } else {
        number--;
     } 
     activeSlide(number);
-    console.log(number);
 }
+const dots = document.querySelectorAll('.slider__dot');// все точки 
+function activeDot() {
+    for (i= 0; i < dots.length; i++) {
+    let openSlide = dots[i];
+    let slideNumber = i;
+    openSlide.addEventListener( "click" , function() {
+        activeSlide(slideNumber);
+        number = slideNumber;
+    });
+    }
+}
+activeSlide(number);
+activeDot();
+
