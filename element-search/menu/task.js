23a6b4e3bc580e5b7e945c918aa340cd6c5daadf
SafaryanAlexandr <11220099.as@gmail.com>
2019-09-01 20:56:45 +0300

    Задача №2 вариант2

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,12 +1,19 @@
 const menuElements = document.querySelectorAll('.menu_sub');
+
 for( i = 0; i <  menuElements.length; i++) {
     let menuActive = menuElements[i].closest('li').querySelector('.menu__link');
     let menuElement = menuElements[i];
+    
     menuActive.onclick = function(event) {
         event.preventDefault();
     }
-    menuActive.addEventListener( 'click' , function() {
-        menuElement.className = 'menu menu_sub menu_active';
-        });
-
+    //menuActive.addEventListener( 'mousemove' , function() {//
+       // menuElement.classList.add('menu_active');//
+      //  });//
+   
+        menuActive.addEventListener( 'click' , function() {
+        menuElement.classList.toggle('menu_active');
+         });
+    
+    
 }
