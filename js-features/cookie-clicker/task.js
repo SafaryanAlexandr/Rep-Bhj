59e820f01cd7ff6fea0e525b87c44002a9f797a1
SafaryanAlexandr <11220099.as@gmail.com>
2019-09-12 21:29:56 +0300

    задача №2

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,3 +1,15 @@
 let clickNumber = document.getElementById('clicker__counter');
-console.log(clickNumber);
-document.getElementById('cookie').addEventListener( "click" , function() {clickNumber.textContent ++});
+let clickSpeed = document.getElementById('clicker__time');
+let timeStart = Date.now();
+document.getElementById('cookie').addEventListener( "click" , function() {
+    clickNumber.textContent ++;
+    let timeClick = (Date.now() - timeStart) / 1000;
+    clickSpeed.textContent = (1 / timeClick).toFixed(3);
+    timeStart = Date.now();
+
+    if ( clickNumber.textContent % 2 === 0 ) {
+        document.getElementById('cookie').width = 200;
+    } else {
+        document.getElementById('cookie').width = 300;
+    }
+});
