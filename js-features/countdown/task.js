a8ec49d0c87ba888efe35f937e99526c2d1c72c5
SafaryanAlexandr <11220099.as@gmail.com>
2019-09-19 22:46:37 +0300

    задача 1 конечный вариант

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,6 +1,8 @@
-setInterval(() => {
+const id = setInterval(() => {
   if(document.getElementById('timer').textContent > 0){
   document.getElementById('timer').textContent --;
   } else {
     window.alert('Вы победили в конкурсе!');
+    clearInterval(id);
   }}, 1000);
+  
